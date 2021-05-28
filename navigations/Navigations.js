315123bbd121c4977598c0aad5225c4929f27376
Valentina Soto <valen2298@hotmail.es>
2021-05-28 18:37:56 -0500

    Add Navigation

---
+++ navigations/Navigations.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import RestaurantsStack from './RestaurantsStack'
+import FavoritesStack from './FavoritesStack'
+import TopRestaurantsStack from './TopRestaurantsStack'
+import SearchStack from './SearchStack'
+import AccountStack from './AccountStack'
+
+const Tab = createBottomTabNavigator()
+
+export default function Navigations() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="restaurants"
+                    component={RestaurantsStack}
+                    options={{ title:"Restaurantes" }}
+                /> 
+                <Tab.Screen
+                    name="favorites"
+                    component={FavoritesStack}
+                    options={{ title:"Favoritos" }}
+                />  
+                <Tab.Screen
+                    name="toprestaurants"
+                    component={TopRestaurantsStack}
+                    options={{ title:"Top 5" }}
+                />  
+                <Tab.Screen
+                    name="search"
+                    component={SearchStack}
+                    options={{ title:"Buscar" }}
+                />  
+                <Tab.Screen
+                    name="account"
+                    component={AccountStack}
+                    options={{ title:"Cuentas" }}
+                />                  
+            </Tab.Navigator>
+        </NavigationContainer>
+
+       
+    )
+}
