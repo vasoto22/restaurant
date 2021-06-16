165233c2fdf114e2b9b601c98a2e92b0eadc4d99
Valentina Soto <valen2298@hotmail.es>
2021-06-15 22:03:27 -0500

    Update Profile Photo

--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -1,25 +1,71 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
+import Toast from 'react-native-easy-toast'
 
-import { closeSession, isUserLogged } from '../../utils/actions'
-import UserGuest from './UserGuest'
+import { closeSession, getCurrentUser, isUserLogged } from '../../utils/actions'
+import Loading from '../../components/Loading'
+import InfoUser from '../../components/account/InfoUser'
 
 export default function UserLogged() {
+    const toastRef = useRef()
     const navigation = useNavigation()
+
+    const [loading, setLoading] = useState(false)
+    const [loadingText, setLoadingText] = useState("")
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+       setUser(getCurrentUser())
+    }, [])
+
     return (
-        <View>
-            <Text>UserLogged...</Text>
+        <View style={styles.container}>
+            {
+                user && ( 
+                    <View>
+                        <InfoUser
+                             user={user} 
+                             setLoading={setLoading}
+                             setLoadingText={setLoadingText}
+                        />
+                        <Text>Account options</Text>
+                    </View>
+                )
+            }
             <Button
                 title="Cerrar sesión"
+                buttonStyle={styles.btnCloseSession}
+                titleStyle={styles.btnCloseSessionTitle}
                 onPress={() => {
                     closeSession()
                     navigation.navigate("restaurants")
                 }}
             />
+            <Toast ref={toastRef} position="center" opacity={0.9}/>
+            <Loading isVisible={loading} text={loadingText}/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        minHeight: "100%",
+        backgroundColor: "#f9f9f9"
+    },
+    btnCloseSession:{
+        marginTop:30,
+        borderRadius: 5,
+        backgroundColor: "#FFFFFF",
+        borderTopWidth: 1,
+        borderTopColor: "#442484",
+        borderBottomWidth: 1,
+        borderBottomColor: "#442484",
+        paddingVertical: 10
+    
+    },
+    btnCloseSessionTitle:{
+        color: "#442484"
+    }
+})
--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,7 @@
 import { firebaseapp } from './firebase'
 import * as firebase from 'firebase'
 import 'firebase/firestore'
+import { fileToBlob } from './helpers'
 
 const db = firebase.firestore(firebaseapp)
 
@@ -44,3 +45,30 @@ export const loginWithEmailAndPassword = async(email, password) => {
     return result
 }
 
+export const uploadImage = async(image, path, name) => {
+    const result = { statusResponse: false, error: null, url: null}
+    const ref = firebase.storage().ref(path).child(name)
+    const blob = await fileToBlob(image)
+
+    try {
+        await ref.put(blob)
+        const url = await firebase.storage().ref(`${path}/${name}`).getDownloadURL()
+        result.statusResponse = true
+        result.url = url
+    } catch (error) {
+        result.error = error
+    }
+    return result
+} 
+
+export const updateProfile = async(data) => {
+    const result = { statusResponse: true, error: null}
+
+    try {
+        await firebase.auth().currentUser.updateProfile(data)
+    } catch (error) {
+        resylt.statusResponse = false
+        result.error = error
+    }
+    return result
+}
